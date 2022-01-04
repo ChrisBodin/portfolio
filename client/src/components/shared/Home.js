3639d343251e12ec1b3a1426c7d58665078ec454
chris bodin <xtropicalmuffin@gmail.com>
2022-01-03 18:03:36 -0700

    final

--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -2,7 +2,9 @@ const Home = () => {
   return (
     <>
     <h1>Home Page</h1>
-    <p>Hello my name is Christopher and a few things about me are that i like to play Games, Rock climb, try food, and go Bowling</p>
+    <p>Hello my name is Christopher and a few things about me are that i like to play Games, Rock climb, try food, and go Bowling.
+      I work at walmart currently and hope to become a backend web developer and and front end web developer to start.
+    </p>
     </>
   )
 }
